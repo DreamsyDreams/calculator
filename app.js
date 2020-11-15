5e27a89370b5ce03052f901f407319eb6c74888e
Valentyn Savich <vsavich1289@gmail.com>
2020-11-15 12:35:29 -0500

    Added variables

---
+++ app.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operation');
+const allClearButton = document.querySelector('.all-clear');
+const deleteButton = document.querySelector('.delete');
+const equalsButton = document.querySelector('.equals');
+const previousOutputText = document.querySelector('.previous-output');
+const currentOutputText = document.querySelector('.current-output');
