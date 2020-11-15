e5bc3ae785776d7a18959b5219d6c2d7c7a3721a
Valentyn Savich <vsavich1289@gmail.com>
2020-11-15 17:32:58 -0500

    Added the Calculator class

--- app.js
+++ app.js
@@ -5,3 +5,36 @@ const deleteButton = document.querySelector('.delete');
 const equalsButton = document.querySelector('.equals');
 const previousOutputText = document.querySelector('.previous-output');
 const currentOutputText = document.querySelector('.current-output');
+
+class Calculator {
+    constructor(previousOutputText, currentOutputText) {
+        this.previousOutputText = previousOutputText;
+        this.currentOutputText = currentOutputText;
+    }
+
+    clear() {
+
+    }
+
+    delete() {
+
+    }
+
+    addNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
+
+const calculator = new Calculator(previousOutputText, currentOutputText);
